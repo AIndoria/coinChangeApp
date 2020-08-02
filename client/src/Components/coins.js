41dc46574c375e226442f6eb7c0eea5efd1cc33a
Abhishek Indoria <indoriaabhishek@gmail.com>
2020-08-02 05:02:24 -0700

    Final Formatting Push

--- client/src/Components/coins.js
+++ client/src/Components/coins.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Styles/coins.css";
 
 class Coins extends React.Component {
   constructor(props) {
@@ -7,7 +8,7 @@ class Coins extends React.Component {
     this.state = {
       coinsSum: 0,
       changeReceived: [],
-      isLoaded: false,
+      isHidden: true,
     };
   }
 
@@ -37,29 +38,97 @@ class Coins extends React.Component {
   };
 
   render() {
+    const { isHidden } = this.state;
+
     return (
-      <div>
-        <h2>Coiny McCoinFace</h2>
-        <form onSubmit={this.handleSubmission.bind(this)}>
-          <label>
-            Value:
-            <input
-              type="text"
-              name="val"
-              pattern="[0-9]*[.]?[0-9]{0,2}"
-              value={this.state.coinsSum}
-              onChange={this.handleValueChange}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        <div>{this.state.coinsSum}</div>
-        <div>{this.state.changeReceived.dollars}</div>
-        <div>{this.state.changeReceived.halfdollars}</div>
-        <div>{this.state.changeReceived.quarters}</div>
-        <div>{this.state.changeReceived.dimes}</div>
-        <div>{this.state.changeReceived.nickles}</div>
-        <div>{this.state.changeReceived.pennies}</div>
+      <div className="Landing">
+        <div className="Landing__CoinInput">
+          <div className="Landing__Heading">
+            <h2>Change Counter</h2>
+          </div>
+          <form
+            className="Landing__CoinInput_form"
+            onSubmit={this.handleSubmission.bind(this)}
+          >
+            <label className="Landing__CoinInput--label">
+              Please enter a sum:
+            </label>
+            <div className="Landing__CoinInput--inputField">
+              <input
+                type="text"
+                name="val"
+                pattern="[0-9]*[.]?[0-9]{0,2}"
+                value={this.state.coinsSum}
+                onChange={this.handleValueChange}
+              />
+            </div>
+            <div className="Landing__CoinInput--populatedCurrency">
+              <span>Change will be shown for: ${this.state.coinsSum}</span>
+            </div>
+            <div className="Landing__CoinInput--submit">
+              <input
+                type="submit"
+                value="Submit"
+                onClick={() => this.setState({ isHidden: false })}
+              />
+            </div>
+          </form>
+        </div>
+
+        {isHidden ? null : (
+          <div className="Landing__CoinOutput" id="Output">
+            <div className="Landing__CoinOutput_values">
+              <div className="Landing__CoinOutput--dollars common-CO-values">
+                <div className="Landing__CoinOutput--dollars-label common-CO-labels">
+                  Silver-Dollar:
+                </div>
+                <div className="Landing__CoinOutput--value">
+                  {this.state.changeReceived.dollars}
+                </div>
+              </div>
+              <div className="Landing__CoinOutput--hdollars value-light common-CO-values">
+                <div className="Landing__CoinOutput--hdollars-label common-CO-labels">
+                  Half-Dollar:
+                </div>
+                <div className="Landing__CoinOutput--value">
+                  {this.state.changeReceived.halfdollars}
+                </div>
+              </div>
+              <div className="Landing__CoinOutput--quarters common-CO-values">
+                <div className="Landing__CoinOutput--quarters-label common-CO-labels">
+                  Quarters:
+                </div>
+                <div className="Landing__CoinOutput--value">
+                  {this.state.changeReceived.quarters}
+                </div>
+              </div>
+              <div className="Landing__CoinOutput--dimes value-light common-CO-values">
+                <div className="Landing__CoinOutput--dimes-label common-CO-labels">
+                  Dimes:
+                </div>
+                <div className="Landing__CoinOutput--value">
+                  {this.state.changeReceived.dimes}
+                </div>
+              </div>
+              <div className="Landing__CoinOutput--nickles common-CO-values">
+                <div className="Landing__CoinOutput--nickles-label common-CO-labels">
+                  Nickles:
+                </div>
+                <div className="Landing__CoinOutput--value">
+                  {this.state.changeReceived.nickles}
+                </div>
+              </div>
+              <div className="Landing__CoinOutput--pennies value-light common-CO-values">
+                <div className="Landing__CoinOutput--pennies-label common-CO-labels">
+                  Pennies:
+                </div>
+                <div className="Landing__CoinOutput--value">
+                  {this.state.changeReceived.pennies}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
