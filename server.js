db92868a98bbfaa332c57a36782e5af0644c732a
Abhishek Indoria <indoriaabhishek@gmail.com>
2020-08-02 01:23:06 -0700

    Modify POST/GET Calls

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
-let totalSum = 200;
+let totalSum = 0;
+const accurateChange = require("./scripts/makeChange");
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.post("/coinvalue", function (req, res) {
-  res.send("Received Total Sum: " + req.body.name + " .");
-  totalSum = req.body.name;
-  console.log(req);
+  console.log(req.body.coinsSum);
+  totalSum = req.body.coinsSum;
 });
 
 app.get("/api/sum", function (req, res) {
-  res.json(totalSum);
+  res.json(accurateChange.makeChange(totalSum));
+  console.log("Total Sum is: " + totalSum);
 });
 
 app.listen(port, () => console.log(`Started Server on Port ${port}`));
